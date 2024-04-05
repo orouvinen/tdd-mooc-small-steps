0e1c5565cfa09950e9a53217fa25d19bd277d0e0
Olli Rouvinen <olli.rouvinen@op.fi>
2024-04-05 11:34:38 +0300

    Refactoring step 7

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -21,7 +21,8 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string);
-    const cost = calculateCost(age, type, date, baseCost);
+    const plainDate = date ? convertDate(date) : undefined;
+    const cost = calculateCost(age, type, plainDate, baseCost);
     res.json({ cost });
   });
 
@@ -31,11 +32,11 @@ function createApp(database: Database) {
     }
   }
 
-  function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number) {
+  function calculateCost(age: number | undefined, type: string, date: Temporal.PlainDate | undefined, baseCost: number) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date && convertDate(date), baseCost);
+      return calculateCostForDayTicket(age, date, baseCost);
     }
   }
 
